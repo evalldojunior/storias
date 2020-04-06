2d82ce4f6f1be929c252aca914995f4789efcce0
evalldojunior <evalldojn@gmail.com>
2020-04-06 18:49:50 -0300

    implementando a collectionView

--- storias.playground/Contents.swift
+++ storias.playground/Contents.swift
@@ -1,14 +1,20 @@
 //: A UIKit based Playground for presenting user interface
-  
+
 import UIKit
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
-
+class MyViewController : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
     let infoButton = UIButton()
     let sairButton = UIButton()
     let viewInfo = UIView()
     let testButton = UIButton()
+    let fotos: [UIImage] = [UIImage(named: "foto1")!, UIImage(named: "foto2")!]
+    let test1 = UICollectionView(frame: CGRect(x: 38, y: 204, width: 1365, height: 591), collectionViewLayout: UICollectionViewFlowLayout())
+    
+    
+    
     
     override func loadView() {
         //view total
@@ -22,13 +28,12 @@ class MyViewController : UIViewController {
         logo.frame = CGRect(x: 85, y: 13, width: 429, height: 173)
         infoButton.frame = CGRect(x: 1275, y: 90, width: 32, height: 32)
         infoButton.setBackgroundImage(UIImage(named: "info"), for: .normal)
-
+        
         // view para a info sobre o playground
         sairButton.frame = CGRect(x: 910, y: 290, width: 45, height: 44)
         sairButton.setBackgroundImage(UIImage(named: "sair"), for: .normal)
         let infos = UIImageView(image: UIImage(named: "infos"))
         infos.frame = CGRect(x: 465, y: 271, width: 510, height: 358)
-        
         let viewTest = UIView()
         viewTest.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.4)
         viewTest.frame.size = CGSize(width: 1440, height: 900)
@@ -46,10 +51,20 @@ class MyViewController : UIViewController {
         testButton.layer.cornerRadius = 25
         testButton.addTarget(nil, action: #selector(MyViewController.testar), for: .touchUpInside)
         
-       
+        //colection view
+        test1.backgroundColor = #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
+        test1.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        if let layout = test1.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+        test1.delegate = self
+        test1.dataSource = self
+        
+        //adicionando elementos na view geral
         view.addSubview(logo)
         view.addSubview(infoButton)
         view.addSubview(testButton) //apagar depois de tirar o test
+        view.addSubview(test1)
         view.addSubview(viewInfo)
         
         self.view = view
@@ -78,6 +93,25 @@ class MyViewController : UIViewController {
     
     @IBAction func testar() {
         navigationController?.show(contoViewController, sender: nil)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 913, height: 591)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fotos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundView = UIImageView(image: fotos[indexPath.item])
+        let titulo = UILabel()
+        titulo.text = "Fotografia do bem"
+        let descricao = UILabel()
+        descricao.text = "Uma história sobre crianças com necessidades especiais retratadas a mostrar diversas formas de ser feliz."
+        //terminando de escrever
+        return cell
     }
 }
 
